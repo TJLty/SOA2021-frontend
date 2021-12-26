0e4fa98318cb0a428bdd17eaab4321b9c8c99eeb
GreymaneCode <421789165@qq.com>
2021-12-26 17:32:06 +0800

    Update hospital_login.vue

--- src/views/hospital_login.vue
+++ src/views/hospital_login.vue
@@ -305,7 +305,7 @@ export default {
         localStorage.removeItem("h_satoken");
         localStorage.setItem("h_satoken", this.satoken);
         console.log(res.data.tokenValue);
-        this.$router.push("/doctor-manage");
+        this.$router.push("/doctor");
       }
     },
     // async register() {
